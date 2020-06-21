c6834c0575fb1b6c0cf64b76c6124bacf3daae0d
Chandler Keep <chandlersamkeep@gmail.com>
2020-06-21 22:51:34 +0100

    v1.0 Station
    - Displaying temp reading to leds

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 #include "Microbit.h"
 
 MicroBit uBit;
+MicroBitStorage storage;
+MicroBitThermometer thermometer(storage);
 
 int main() {
 	uBit.init();
 	while(true){
 		uBit.display.scroll("HELLO");
+		uBit.display.scroll(thermometer.getTemperature());
 	}
 	release_fiber();
 }
--- README.md
+++ README.md
@@ -7,3 +7,5 @@ yotta init
 yotta target bbc-microbit-classic-gcc
 yotta install lancaster-university/microbit
 yotta build
+
+https://lancaster-university.github.io/microbit-docs/offline-toolchains/
