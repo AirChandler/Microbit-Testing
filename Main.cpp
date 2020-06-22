666e06e1c66a6995584af2d41506bc969c8b6e5b
Chandler Keep <chandlersamkeep@gmail.com>
2020-06-22 17:40:01 +0100

    v1.1 Serial support
    - Added usb serial support, compatible with any serial monitors

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,17 @@
+// Microbit fun, Chandler Keep, 22/06/2020
 #include "Microbit.h"
 
 MicroBit uBit;
 MicroBitStorage storage;
+MicroBitSerial serial(USBTX, USBRX); //Transmitter/Receiver ref.
 MicroBitThermometer thermometer(storage);
 
 int main() {
-	uBit.init();
+	uBit.init(); //Initialises thread
 	while(true){
 		uBit.display.scroll("HELLO");
+		serial.send("Hello\n");
 		uBit.display.scroll(thermometer.getTemperature());
 	}
-	release_fiber();
+	release_fiber(); //Releases the thread
 }
